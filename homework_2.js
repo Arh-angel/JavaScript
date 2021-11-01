8743c9938d9412b3c33e17a41e62d1640f1a038d
Kholev Alexandr <khalex8993@gmail.com>
2021-11-01 18:15:29 +0300

    homework_2

---
+++ homework_2.js
@@ -0,0 +1,17 @@
+"кот" > "код" 
+"2" + 2 * "2"
+undefined == null
+undefined === null
+null == 0
+2 > "3"
+null - false + true
+1 / "l"
+"2" * "3"
+4 + 5 + "O"
+"l" + 4 + 5
+"4" - 2
+"4" - "4x"
+null == false
+" -4 "/ 0 + 1
+null + 1
+undefined + null
